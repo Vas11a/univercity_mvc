91b9ab09b6af0e2d5626d28427e0038d390a189e
Vasyl <vasapanov721@gmail.com>
2024-04-21 19:29:19 +0200

    poprawa

---
+++ poprawa/lab1/app.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const { getHTMLDocumentStart, getHTMLDocumentEnd } = require('./htmlGenerator');
+const { getCars, getCarInformation, getCarAge } = require('./cars');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    console.log(`Server is running on ${PORT}.`);
+    const carsList = getCars();
+    console.log(carsList);
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(getHTMLDocumentStart());
+    res.write('<body>');
+    res.write('<p>' + getCarInformation(3) + '</p>');
+    res.write('<p>' + getCarAge(2) + '</p>');
+    res.write('</body>');
+    res.write(getHTMLDocumentEnd());
+    res.end();
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
